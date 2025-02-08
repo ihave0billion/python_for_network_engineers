c9842bbccc4d9367b3c47747e48c8c0bbd5c779a
ihave0billion <21garcia21@gmail.com>
2025-02-08 15:06:59 -0500

    add float and integers

---
+++ float.py
@@ -0,0 +1,11 @@
+#a floating point is a whole number with a decimal place
+#floats can also use 'e' for exponential values
+
+
+print("4.5")
+print(type("4.5"))
+print(float("4.5"))
+print(type(float("4.5")))
+
+print(1e6)
+print(type(1e6))
---
+++ integers.py
@@ -0,0 +1,16 @@
+#integer is a whole number with no decimal places
+print(3)
+print(-3)
+print(200378)
+print(-200_378)
+print(25_000_000)
+print(type(-25_000_000))
+
+#use underscores in place of commas in python to make it human readable
+
+#string to integers
+print("string to integers")
+print("23")
+print(type("23"))
+print(int("23"))
+print(type(int("23")))
